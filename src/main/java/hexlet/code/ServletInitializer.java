117e88856b05fb93121bfa6c964de6b08ea417a6
maddojka <yuriy.soroko@gmail.com>
2025-11-20 07:03:30 +0300

    checkstyle + gradle

--- src/main/java/hexlet/code/ServletInitializer.java
+++ src/main/java/hexlet/code/ServletInitializer.java
@@ -1,4 +1,4 @@
-package hexlet.code.app;
+package hexlet.code;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
